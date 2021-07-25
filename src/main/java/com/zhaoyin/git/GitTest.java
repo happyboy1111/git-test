cfcdb41240c4579f94ad8d18e749f3bac85ec1a2
Lipengfei <Lipengfei@zhaoyin.com>
2021-07-26 00:03:20 +0800

    master push to github2

--- src/main/java/com/zhaoyin/git/GitTest.java
+++ src/main/java/com/zhaoyin/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("master push to github");
+        System.out.println("master push to github2");
     }
 }
