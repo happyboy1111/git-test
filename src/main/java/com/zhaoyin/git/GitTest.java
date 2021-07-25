7e970ef26bc89771dd51d9de31621c95b53cf65f
happyboy1111 <87924853+happyboy1111@users.noreply.github.com>
2021-07-26 00:08:05 +0800

    pull test

--- src/main/java/com/zhaoyin/git/GitTest.java
+++ src/main/java/com/zhaoyin/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master push to github");
         System.out.println("master push to github2");
+        System.out.println("master push to github3");
     }
 }
